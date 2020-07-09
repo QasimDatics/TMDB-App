7d42ee5e48e3420289ca12fd1de931c460a3fc1f
QasimDatics <qasim.ali@datics.ai>
2020-07-09 21:37:03 +0500

    working with movies and detail but crashes

--- app/src/main/java/com/example/tmdbapps/detail/DetailActivity.kt
+++ app/src/main/java/com/example/tmdbapps/detail/DetailActivity.kt
@@ -1,15 +1,54 @@
 package com.example.tmdbapps.detail
 
+import android.icu.text.CaseMap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+import com.example.tmdbapps.BuildConfig.URL_POSTER
+import com.squareup.picasso.Picasso
+import org.jetbrains.anko.*
+import org.w3c.dom.Text
 
+@Suppress("NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS")
 class DetailActivity : AppCompatActivity() {
 
     private var titleMovie:String=""
     private var posterMovie:String=""
     private var overviewMovie:String=""
 
+    private lateinit var poster:ImageView
+    private lateinit var title: TextView
+    private lateinit var overview:TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        linearLayout{
+            lparams(width= matchParent,height = wrapContent)
+            orientation=LinearLayout.VERTICAL
+            padding=dip(16)
+
+            poster=imageView{
+
+            }.lparams{
+                width=dip(250)
+                gravity=Gravity.CENTER
+                height=dip(250)
+            }
+            title=textView()
+            overview=textView()
+
+        }
+        val i=intent
+        titleMovie = i.getStringExtra("TITLE")
+        overviewMovie = i.getStringExtra("OVERVIEW")
+        posterMovie = i.getStringExtra("POSTER")
+
+        title.text=titleMovie
+        overview.text=overviewMovie
+        Picasso.get().load(URL_POSTER + posterMovie).into(poster)
     }
 }
