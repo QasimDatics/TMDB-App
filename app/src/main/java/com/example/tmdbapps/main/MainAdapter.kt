735a0b3e9db2e10d50fbcb42eb490d6764ab8747
QasimDatics <qasim.ali@datics.ai>
2020-07-09 13:23:53 +0500

    TMDB

--- app/src/main/java/com/example/tmdbapps/main/MainAdapter.kt
+++ app/src/main/java/com/example/tmdbapps/main/MainAdapter.kt
@@ -1,4 +1,33 @@
 package com.example.tmdbapps.main
 
-class MainAdapter {
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.tmdbapps.model.Movie
+import org.jetbrains.anko.AnkoComponent
+import org.jetbrains.anko.AnkoContext
+import java.util.*
+
+class MainAdapter(private val result: List<Movie>, private val listener: (Movie)->Unit):RecyclerView.Adapter<MovieViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
+    }
+
+    override fun getItemCount(): Int {
+    }
+
+    override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
+    }
+
+}
+class MovieUi:AnkoComponent<ViewGroup>{
+    override fun createView(ui: AnkoContext<ViewGroup>): View {
+        return with(ui){
+
+        }
+    }
+}
+class MovieViewHolder(view: View):RecyclerView.ViewHolder(view){
+    fun bindItem(movies:Movie, listener: (Movie) -> Unit){
+
+    }
 }
